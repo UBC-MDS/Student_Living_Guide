71d8208b0dd31f6eeb0bcf4fe26bcd7f3573004e
flora19 <flora19@student.ubc.ca>
2023-03-15 21:12:06 -0700

    fixing typos

--- Student_Living_Guide_Shiny/server/tab1.R
+++ Student_Living_Guide_Shiny/server/tab1.R
@@ -9,6 +9,7 @@ lower_data =  tail(data_arranged , n = 10)
 up_data = head(data_arranged , n = 10)
 
 up_data$Country <- factor(up_data$Country, levels = unique(up_data$Country)[order(up_data$Cost.of.Living.Index, decreasing = FALSE)])
+
 lower_data$Country <-factor(lower_data$Country, levels = unique(lower_data$Country)[order(lower_data$Cost.of.Living.Index, decreasing = FALSE)])
 
 
@@ -32,7 +33,7 @@ filtered_df <- reactive({
                    groceries_index = `Groceries.Index`,
                    rest_price_index = `Restaurant.Price.Index`,
                    purchasing_power_index = `Local.Purchasing.Power.Index`))
-
+  
 })
 
 filtered_df_country <- reactive({
@@ -59,7 +60,7 @@ mean_col_continent <- reactive({
 
 # observe component
 observe({
-
+  
   # reactive element 1: select all continent if "select all" checked
   observeEvent(input$all_cont_checkbox, {
     if (is.null(input$all_cont_checkbox)) {
@@ -73,7 +74,7 @@ observe({
       selected = selected_
     )
   }, ignoreNULL = FALSE)
-
+  
   # reactive element 2: un-select "select_all" if no continent selected
   observeEvent(input$continent_select, {
     if (is.null(input$continent_select)) {
@@ -85,7 +86,7 @@ observe({
       )
     }
   }, ignoreNULL = FALSE)
-
+  
 })
 
 # Render the table output
@@ -103,15 +104,15 @@ output$map1 <- renderLeaflet({
               length.out = 11)
   colors_0_100 <- colorRampPalette(c("blue", "purple"))(99)
   colors_0_100_200 <- c(colors_0_100, "black", rev(colorRampPalette(c("red", "yellow"))(99)))
-
+  
   pal <- colorBin(colors_0_100_200, domain = filtered_df$cost_living, bins = bins)
-
+  
   # define data point label
   labels <- paste("<p><b> Continent </b>: ", filtered_df$Continent, "</p>",
                   "<p><b> Country </b>: ", filtered_df$Country, "</p>",
                   "<p><b> Cost of Living Index </b>: ", filtered_df$cost_living, "</p>",
                   sep="")
-
+  
   leaflet() %>%
     addTiles(urlTemplate="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")%>%
     addRectangles(lng1 = filtered_df_country$longitude - 2, lat1 = filtered_df_country$latitude - 2,
@@ -139,24 +140,40 @@ observeEvent(input$map1_marker_click, {
 
 # bar plot 1
 output$barPlot1 <- renderPlotly({
-
+  # get filtered data
+  filtered_df <- filtered_df()
+  data_arranged <- filtered_df|>
+    arrange(desc(`cost_living`))
+  
+  up_data = head(data_arranged , n = 10)
+  up_data$Country <- factor(up_data$Country, levels = unique(up_data$Country)[order(up_data$cost_living, decreasing = FALSE)])
   # ========================
   # code below for bar plot
   # ========================
-  plot_ly(up_data, x = ~Cost.of.Living.Index, y = ~Country, type = 'bar') %>%
-    layout(title = "The 10 most expensive countries", xaxis = list(title = "Cost index"), yaxis = list(title = "Country"))
+  plot_ly(up_data, x = ~cost_living, y = ~Country, type = 'bar') %>%
+    layout(title = paste("The ", nrow(up_data), " most expensive countries"), xaxis = list(title = "Cost index"), yaxis = list(title = "Country"))
 })
 br()
 # bar plot 2
 output$barPlot2 <- renderPlotly({
-
+  # get filtered data
+  filtered_df <- filtered_df()
+  data_arranged <- filtered_df|>
+    arrange(desc(`cost_living`))
+  
+  lower_data = tail(data_arranged , n = 10)
+  lower_data$Country <- factor(lower_data$Country, levels = unique(lower_data$Country)[order(lower_data$cost_living, decreasing = FALSE)])
+  
+  
   # ========================
   # code below for bar plot
   # ========================
-  plot_ly(lower_data, x = ~Cost.of.Living.Index, y = ~Country, type = 'bar') %>%
-    layout(title = "The 10 least expensive countries",
+  plot_ly(lower_data, x = ~cost_living, y = ~Country, type = 'bar') %>%
+    layout(title = paste("The ", nrow(lower_data), " least expensive countries"),
            xaxis = list(title = "Cost index"),  yaxis = list(title = "Country"))
 })
+
+
 
 # ========================
 # code below for distribution plot
@@ -165,7 +182,7 @@ output$distplot1 <- renderPlotly({
   filtered_df <- filtered_df()
   filtered_df_country <- filtered_df_country()
   mean_col_continent <- mean_col_continent()
-
+  
   # continent cost of living mean value
   vline <- function(x = 0, color = "green") {
     list(
@@ -190,20 +207,20 @@ output$distplot1 <- renderPlotly({
       line = list(color = color, dash = "dot")
     )
   }
-
+  
   gg <- ggplot(filtered_df, aes(x = cost_living)) +
     geom_histogram(aes(y = after_stat(density)), color = "white", fill = "#1F77B4", binwidth = 3) +
     geom_density(outline.type = "upper", adjust = 1.75, linewidth = 0.4) +
     geom_vline(aes(xintercept = mean_col_continent),
-       color = "black", linetype = "solid", linewidth = 0.4) +
+               color = "black", linetype = "solid", linewidth = 0.4) +
     # annotate("text", x = mean_col_continent + 12, y = 0.03, label = paste("Mean:", mean_col_continent)) +
     # annotate("text", x = filtered_df_country$cost_living + 12, y = 0.03, label = filtered_df_country$Country) +
     geom_vline(aes(xintercept=filtered_df_country$cost_living),
-                color="red", linetype="dash", linewidth = 0.4) +
+               color="red", linetype="dash", linewidth = 0.4) +
     scale_x_continuous(limits = c(min(filtered_df$cost_living) - 20,max(filtered_df$cost_living) + 20), expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
     theme_bw()
-
+  
   ggplotly(gg) %>%
     layout(
       title = list(text = "Cost of Living Distribution Plot", y = 0.995),
@@ -215,15 +232,15 @@ output$distplot1 <- renderPlotly({
       )
     ) %>%
     add_annotations(x = mean_col_continent, y = 0.038,
-                 text = paste("Mean:", mean_col_continent),
-                 showarrow = FALSE,
-                 xshift = 45, yshift = 10, font = list(size = 12)) %>%
+                    text = paste("Mean:", mean_col_continent),
+                    showarrow = FALSE,
+                    xshift = 45, yshift = 10, font = list(size = 12)) %>%
     add_annotations(
       x = filtered_df_country$cost_living, y = 0.03,
       text = paste(filtered_df_country$Country, '(', filtered_df_country$cost_living ,')'),
       showarrow = FALSE,
       xshift = 70, yshift = 10, font = list(size = 12)
-   )
+    )
 })
 
 # ========================
@@ -268,5 +285,5 @@ output$scatterplot <- renderPlotly({
     layout(title = "Correlation Plot between Indices",
            xaxis = list(title = x_axis_select),
            yaxis = list(title = y_axis_select))
-
+  
 })
